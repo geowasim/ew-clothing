7c0c00c71b19af6457f40d779f122a8f322fc8e2
Wasim <waseem_wmgh@yahoo.com>
2020-05-11 21:27:03 +0200

    update our menu-item to naviage withRouter HOC

--- src/components/directory/direcotry.component.jsx
+++ src/components/directory/direcotry.component.jsx
@@ -13,33 +13,33 @@ class Directory extends React.Component {
             title: 'HATS',
             imageUrl: 'https://images.unsplash.com/photo-1572460737616-866a25d4d61a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
             id: 1,
-            linkUrl: 'shop/hats'
+            linkUrl: 'hats' //'shop/hats'
           },
           {
             title: 'JACKETS',
             imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
             id: 2,
-            linkUrl: 'shop/jackets'
+            linkUrl: 'jackets'//shop/jackets
           },
           {
             title: 'SNEAKERS',
             imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
             id: 3,
-            linkUrl: 'shop/sneakers'
+            linkUrl: ''//shop/sneakers
           },
           {
             title: 'WOMEN',
             imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
             size: 'large',
             id: 4,
-            linkUrl: 'shop/womens'
+            linkUrl: ''//shop/womens
           },
           {
             title: 'MEN',
             imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
             size: 'large',
             id: 5,
-            linkUrl: 'shop/mens'
+            linkUrl: ''//shop/mens
           }
         ]   
     }
@@ -48,8 +48,8 @@ class Directory extends React.Component {
     return(
       <div className='directory-menu'>
         {
-          this.state.sections.map(({title, imageUrl, id, size})=>(
-            <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+          this.state.sections.map(({id,...otherSectionProps})=>(
+            <MenuItem key={id} {...otherSectionProps}/>
           ))
         } 
       </div>
