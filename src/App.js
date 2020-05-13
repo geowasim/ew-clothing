7a0e65e209ba6f52f69e03ed38f0518b0ac5bf20
Wasim <waseem_wmgh@yahoo.com>
2020-05-13 23:19:45 +0200

    Adding shop & collection components , and header for all pages and links for homepage and shop

--- src/App.js
+++ src/App.js
@@ -3,20 +3,17 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom'
 
 import HomePage from './pages/homepage/homepage.component';
+import Shop from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
-const HatsPage =(props)=>{ 
-return (
-  <div>
-    <h1> HATS PAGE </h1>
-  </div>
-);}
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route  path='/hats' component={HatsPage} />
+        <Route  path='/shop' component={Shop} />
       </Switch>
     </div>
   );
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Directory from '../../components/directory/direcotry.component'
 import './homepage.styles.scss';
 
-const HomePage = ()=> (
+const HomePage = () => (
   <div className='homepage'>
     <h1>HomePage</h1>
     <Directory />
